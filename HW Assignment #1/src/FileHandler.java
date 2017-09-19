24965b8b13beda9ad97fe7fdeca51201ada0fb5f
MurphyP1 <MurphyP1@104-10-2017.bi.local>
2017-09-19 12:23:20 -0400

    Post Class

--- HW Assignment #1/src/FileHandler.java
+++ HW Assignment #1/src/FileHandler.java
@@ -1,21 +1,54 @@
+import java.io.File;
 import java.io.*;
+import java.util.Scanner;
 
 public class FileHandler {
-	//Take a file name 
-	private String path; 
-	private File file;
-	
-	public FileHandler() {
-		path = "/../Datawords.text"; //..means go up a dir
-		file = new File(path);
+	// Method that returWns scanner of words.txt
+	// Method that returns a PrintWriter of ramblex dictionary.java
+
+	// NEED TO MAKE A FORMATTIG METHOD WHICH WILL BE SENT INTO THE RAMBLEX FILE
+
+	public Scanner openWords(String fname) {
+		File file = new File(fname);
+		Scanner input = null;
+
+		try {
+			input = new Scanner(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("error u goob, no can do bc of " + ex.toString());
+			return null;
+		}
+		
+		return input;
 	}
 
-}
+	public PrintWriter openDictionary(String fname) {
+		File file = new File(fname);
+		PrintWriter output = null;
 
-/*READING FROM A TEXT FILE
- * try {
- * 	input = new Scanner(file);  Instead of reading from kb, reads from file
- * } catch {FileNotFoundException ex) {
- * 	System.out.println("** CANNOT OPEN " + path + " ***);
- * } 
- */
+		try {
+			output = new PrintWriter(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("error u goob, no can do bc of " + ex.toString());
+			return null;
+		}
+		return output;
+	}
+
+	public void writeEntry(Scanner input, PrintWriter output) {
+		while (input.hasNextLine()) {
+			String word = input.nextLine().toUpperCase(); // Smart Programming!
+
+			if (word.length() >= 3 && word.length() <= 5) {
+				output.println("\t\t\"" + word + "\",");
+			}
+		}
+	}
+	
+	public void writeEntryHeader(PrintWriter output) {
+		output.println("public class RamblexDictionary { \n\tprivate String[] words = {");
+	}
+	public void writeEntryFooter(PrintWriter output) {
+		output.println("\t};\n}");
+	}
+}
--- HW Assignment #1/src/IOTester.java
+++ HW Assignment #1/src/IOTester.java
@@ -1,9 +1,83 @@
+import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class IOTester {
+	// Method that returns scanner of words.txt
+	// Method that returns a PrintWriter of ramblex dictionary.java
+
+	// NEED TO MAKE A FORMATTIG METHOD WHICH WILL BE SENT INTO THE RAMBLEX FILE
+
+	public static Scanner openWords(String fname) {
+		File file = new File(fname);
+		Scanner input = null;
+
+		try {
+			input = new Scanner(file);
+			//CHECK FOR BALANCED BRACES
+		} catch (FileNotFoundException ex) {
+			System.out.println("error u goob, no can do bc of " + ex.toString());
+			//PROVIDE THIS MESSAGE TO THE OUTPUT FILE: "Part 1: Unable to Open File"
+			return null;
+		}
+		
+		return input;
+	}
+
+	public static PrintWriter openDictionary(String fname) {
+		File file = new File(fname);
+		PrintWriter output = null;
+
+		try {
+			output = new PrintWriter(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("error u goob, no can do bc of " + ex.toString());
+			return null;
+		}
+		return output;
+	}
+
+	public static void writeEntry(Scanner input, PrintWriter output) {
+		//DISPLAY A BLANK LIN IN THE OUTPUT FILE
+		while (input.hasNextLine()) {
+			String word = input.nextLine().toUpperCase(); // Smart Programming!
+
+			if (word.length() >= 3 && word.length() <= 5) {
+				output.println("\t\t\"" + word + "\",");
+			}
+		}
+	}
+	
+	public static void writeEntryHeader(PrintWriter output) {
+		output.println("public class RamblexDictionary { \n\tprivate String[] words = {");
+	}
+	public static void writeEntryFooter(PrintWriter output) {
+		output.println("\t};\n}");
+	}
+	
+	
+	//COMPARE TWO FILES
+	
+	//CREATE ADLIB FROM 3RD ARGUMENT
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if (args.length < 2) { // Need two args, input and output files
+			System.out.println("No file given, not enough args");
+			System.exit(1);
+		}
+		Scanner in = openWords(args[0]); // First argument passed in via command line
+		if (in == null)
+			System.exit(1);
+		;
 
+		PrintWriter out = openDictionary(args[1]);
+
+		writeEntryHeader(out);
+		writeEntry(in, out);
+		writeEntryFooter(out);
+		
+		in.close();
+		out.close();
 	}
-
 }
