74110f925648e92d962c982879c6cc4cbedb9c5c
MurphyPone <murphyP1@bishopireton.org>
2017-09-25 22:10:14 -0400

    pre-asking for help

--- HW Assignment #1/src/IOTester.java
+++ HW Assignment #1/src/IOTester.java
@@ -6,7 +6,7 @@
  * 						  	3. Replacing keywords from a given file with words provided 
  * 								by the user
  * @author MurphyP1
- * @date 9/24/17
+ * @date 9/26/17
  */
 
 import java.io.*;
@@ -14,15 +14,14 @@ import java.util.*;
 
 public class IOTester {	
 	
-	//Field for bracket checking
-	public static int numBraces = 0;
+	public static int numBraces = 0; //Field for brace checking
 	
 	/**
 	 * Returns a Scanner from the given filename String depending on whether of not 
 	 * that file exists
 	 * 
 	 * @author MurphyP1
-	 * @date 9/24/17
+	 * @date 9/26/17
 	 * @method openWords
 	 * 
 	 * @param fname the name of the file to open with a Scanner 
@@ -37,7 +36,7 @@ public class IOTester {
 
 		try {							//Check to see if the requested input file exists in the given directory
 			input = new Scanner(file);	//If so, create a new Scanner to grab the data
-		} catch (FileNotFoundException ex) {	//TODO Else, print to console the reason why and create that file with the error message inside
+		} catch (FileNotFoundException ex) {	//Else, print to console the reason why and output the reason why & part of program getting stuck
 			System.out.println("error u goob, cannot open " + fname);		
 			output.print("Part " + fileNum + ": Unable to Open File\n");
 			return null;
@@ -46,20 +45,23 @@ public class IOTester {
 	}
 	
 	/**
-	 * @author MurphyP1
-	 * date 9/24/2017
-	 * @method: braceChecker
+	 * Returns a boolean based on the syntactical validity of the order and number of braces 
 	 * 
-	 * @param: input
-	 * @return: boolean
+	 * @author MurphyP1
+	 * @date 9/26/17
+	 * @method braceChecker
+	 * 
+	 * @param input the Scanner whose braces are checked
+	 * 
+	 * @return true of the braces are valid according Java syntax, false otherwise
 	 */
 	//Brace checker
 	public static boolean braceChecker(Scanner input) {
         String temp;	//Grabs the current line of the input 
         //Running difference between { and }
-        while(input.hasNext()) {
+        while(input.hasNext()) {	//checks if input == null when called
 			temp = input.next();
-			for(int i = 0; i < temp.length(); i++) {  
+			for(int i = 0; i < temp.length(); i++) {	//Iterates through the current line of input
 				//If numBraces ever dips below 0, then they ain't balanced!
 				if(numBraces >= 0 ) { 
 		            if(temp.charAt(i) == '{')
@@ -77,20 +79,22 @@ public class IOTester {
 	}
 	
 	/**
-	 * @author MurphyP1
-	 * date 9/24/2017
-	 * @method: openDictionary
+	 * Returns a PrintWriter object to write to the output file
 	 * 
-	 * @param: fname
-	 * @return: PrintWriter
-	 * @throws FileNotFoundException 
+	 * @author MurphyP1
+	 * @date 9/26/17
+	 * @method writeWords
+	 * 
+	 * @param fname a String to create a file from
+	 * 
+	 * @return a PrintWriter if the file exists and can be opened, otherwise returns null 
 	 */
 	//For writing
-	public static PrintWriter openDictionary(String fname) throws NullPointerException {
+	public static PrintWriter writeWords(String fname)  {
 		File file = new File(fname);
 		PrintWriter output = null;
 
-		try {	//Check to see if the requested input file exists in the given directory
+		try {	//Check to see if the requested file exists/can be opened
 			output = new PrintWriter(file);	//If so, create a new PrintWriter to modify the contents of the file
 		} catch (FileNotFoundException ex) {	//Should not be necessary to include some error output to file bc the PrintWriter should not get created if the file does not exist
 			System.out.println("Failed to be able to write to " + fname + "bc of " + ex.toString());
@@ -100,34 +104,39 @@ public class IOTester {
 	}
 	
 	/**
-	 * @author MurphyP1
-	 * date 9/24/2017
-	 * @method: compareRead
+	 * Analyzes the differences, if any, between two files and writes the result to the output file
 	 * 
-	 * @param: one, two, output
-	 * @return: void
+	 * @author MurphyP1
+	 * @date 9/26/17
+	 * @method compare
+	 * 
+	 * @param one the name of the first file to be created for comparison
+	 * @param two the name of the second file to be created for comparison
+	 * @param output the PrintWriter object to write the results
+	 * 
+	 * @return void
 	 */
 	//COMPARE TWO FILES
-	public static void compareRead(String one, String two, PrintWriter output) {
+	public static void compare(String one, String two, PrintWriter output) {
 		Scanner scanOne = openWords(one, output, 2);	//These share fileNum 2 because this is the second part of the program
 		Scanner scanTwo = openWords(two, output, 2);
 		String strOne;
 		String strTwo;
 												//They have to exist too
 		if( (scanOne != null && scanTwo != null) && (scanOne.hasNext() && scanTwo.hasNext()) ) {	
-			while (scanOne.hasNext()  && scanTwo.hasNext() ) {	//If one File>Scanner has stuff in it, try to compare to the other
-				strOne = scanOne.next();	//Creates strings from scanners from files passed in as arguments
+			while (scanOne.hasNext()  && scanTwo.hasNext() ) {	//If both File>Scanners have stuff in them, try to compare to the other
+				strOne = scanOne.next();	//Creates strings from Scanners from Files from Strings passed in as arguments
 				strTwo = scanTwo.next();
 				
-				for(int i = 0; i < strOne.length(); i++) {  
+				for(int i = 0; i < strOne.length(); i++) {//TODO TEST HERE IF FILE 2 IS LONGER THAN FILE1 WHAT HAPPENS   
 					try {
-						if(strOne.charAt(i) != strTwo.charAt(i) ) {	//If every character is not identical, then the files not
+						if(strOne.charAt(i) != strTwo.charAt(i) ) {	//If every character is not identical, then output not false
 							output.print("Files Not Identical\n");
 		            		scanOne.close();
 		            		scanTwo.close();
 		            		return;
 			            }
-					} catch(StringIndexOutOfBoundsException e) {
+					} catch(StringIndexOutOfBoundsException e) {//Should account for the error above?!!!??
 						output.print("Files Not Identical\n");
 						System.out.println(e + "Therefore they ain't the same");
 		            		scanOne.close();
@@ -136,10 +145,11 @@ public class IOTester {
 					}
 				}
 			}
-			//HAve to be in here bc this is the umbrella of existence
+			//Scanners must close within this brace as this is the umbrella of existence.
 			scanOne.close();
 			scanTwo.close();
-			//This is not the best way to deal with this
+			
+			//TODO This is not the best way to deal with this
 		} else { //Both files not exist and have next then
 			return; //Breakout bc openwords already printed the "Unable nonsense
 		}
@@ -149,12 +159,17 @@ public class IOTester {
 	}
 	
 	/**
-	 * @author MurphyP1
-	 * date 9/24/2017
-	 * @method: prompt
+	 * Gets the the user's input if a file of words is not supplied
 	 * 
-	 * @param: input, kb, output
-	 * @return: ArrayList<String>
+	 * @author MurphyP1
+	 * @date 9/26/17
+	 * @method prompt
+	 * 
+	 * @param input the Scanner which searches for tags, prompting user input
+	 * @param kb the Scanner which gets user input from the keyboard
+	 * @param output the PrintWriter object to write the results
+	 * 
+	 * @return an ArrayList containing strings to replace the words within the tags of the input file
 	 */
 	//Adlib reader
 	public static ArrayList<String> prompt(Scanner input, Scanner kb, PrintWriter output) {
@@ -163,18 +178,18 @@ public class IOTester {
 		//Grab part of speech
 		while(input.hasNextLine() ) {	//Look for the next line
 			String curLine = input.nextLine();
-			String partOfSpeech;
-			String response;
+			String partOfSpeech;		//Contents of the tag
+			String response;			
 			
 			for(int i = 0; i < curLine.length(); i++) {
-				if(curLine.charAt(i) == '<') {
-					partOfSpeech = curLine.substring(curLine.indexOf("<") + 1, curLine.indexOf(">"));
-					curLine = curLine.substring( curLine.indexOf(">")+1); //cuts off everything before the first > hopefully
+				if(curLine.charAt(i) == '<') {	//Doesn't account for lonely <s 
+					partOfSpeech = curLine.substring(curLine.indexOf("<") + 1, curLine.indexOf(">"));	//TODO I feel like this should error out, put it doesn't...
+					curLine = curLine.substring( curLine.indexOf(">")+1); //cuts off everything before the closing >
 					
 					System.out.println("\nGive a " + partOfSpeech);
-					response = kb.nextLine();	//TODO -- (FIXED?) ONLY ACCEPTS 1 WORD RESPONSE
+					response = kb.nextLine();	
 					
-					responses.add(response);	//Inserts response into arraylist of responses
+					responses.add(response);	//Inserts response into ArrayList of responses
 				}
 			}
 		}
@@ -186,22 +201,26 @@ public class IOTester {
 	}
 	
 	/**
-	 * @author MurphyP1
-	 * date 9/24/2017
-	 * @method: writeAdlib
+	 * Writes the new 'adlib' to the output file repalcing the tags with either the user's input, or the prepared file 
 	 * 
-	 * @param: words, input, output
-	 * @return: void
+	 * @author MurphyP1
+	 * @date 9/26/17
+	 * @method writeAdlib
+	 * 
+	 * @param words the ArrayList of words to repalce the tags
+	 * @param input the Scanner which finds the tags 
+	 * @param output the PrintWriter object to write the results
+	 * 
+	 * @return void
 	 */
 	//Adlib Writer				
 	public static void writeAdlib(ArrayList<String> words, Scanner input, PrintWriter output) {
 		int i = 0;	//Keeps track of current word
-		//TODO check size --Don;t remember what I meant????
 		//TODO CHECK IF IT EXISTS --if adlibin.txt DNE, then you need to figure that out asap.
 		while(input.hasNextLine() ) {	//Look for the next line
 			String curLine = input.nextLine(); 	
 		
-			//TODO DOESNT CHANGE TAGS WITH SPACES  --Maybe because it only 
+			//TODO COPY THIS LINE TO PROMPT? ALTHOUGH THERE IS NOT CURRENTLY AN ISSUE
 			while(curLine.indexOf('<') != -1 && curLine.indexOf('>') != -1) { //While there is a valid tag in the line
 				if(i < words.size() ) {	//Checks to see if there are more words to fill
 					curLine = curLine.substring(0,curLine.indexOf('<')) + words.get(i) + curLine.substring(curLine.indexOf('>') +1 );
@@ -209,47 +228,50 @@ public class IOTester {
 				} else { //IF there are fewer responses than tags
 					curLine = curLine.substring(0,curLine.indexOf('<')) + "*NO RESPONSE GIVEN*" + curLine.substring(curLine.indexOf('>') +1 );
 				}
-				//output.println(curLine);
 			}	
 			output.println(curLine);
 		}
 	}
 
 	/**
-	 * @author MurphyP1
-	 * date 9/24/2017
-	 * @method: main
+	 * Executes all of the previously declared methods in a particular order
 	 * 
-	 * @param: args
-	 * @return: void
+	 * @author MurphyP1
+	 * @date 9/26/17
+	 * @method main
+	 * 
+	 * @param args the names of the files to be passed in through the command line
+	 * 
+	 * @return void
 	 */
-	/*Args in should be as follows:
-	 * arg0.java
-	 * file1.txt		//for testing compare
-	 * adlibIn.txt
-	 * AdlibWords.txt 
-	 * output.txt	
+	
+	/*Args in should in the following order:
+	 * arg0.java				//For checking braces and compare
+	 * file1.txt				//For testing compare
+	 * adlibIn.txt			//Input file for the adlib
+	 * AdlibWords.txt 		//Optional file containing prepared responses to adlibIn.txt
+	 * output.txt			//The output file
 	 */
 	public static void main(String[] args) {
 		PrintWriter out; 
 		
-		if (args.length < 4) { // If there is no preppedWords file given
+		if (args.length < 4) { // If there is file of prepared words given
 			//Creates PrintWriter to output to second file 
-			out = openDictionary(args[3]);	//output.txt is 4th argument
+			out = writeWords(args[3]);	//output.txt is 4th argument
 		} else {	// If there is a 4th argument passed
-			out = openDictionary(args[4]);	//output.txt is 5th argument
+			out = writeWords(args[4]);	//output.txt is 5th argument
 		}
 		
-		for(int i = 0; i < args.length; i++) 
-			System.out.println("args[" + i + "] = " + args[i]);
+		for(int i = 0; i < args.length; i++) 	//Prints the arguments for console viewing
+			System.out.println("args[" + i + "] = " + args[i]);	
 	
 	//PART ONE----------------------------------------------------------------
-		//Reads in first File
-		Scanner in = openWords(args[0], out, 1); // First argument passed in via command line
 		
-		//Checks if braces are balanced and modifies 2nd arg file accordingly
+		Scanner in = openWords(args[0], out, 1); //Reads in first File
+		
+		//Checks if braces are balanced and modifies output.txt accordingly
 		if(in != null) {
-			if(braceChecker(in))	//Can only call braceChecker once per Scanner bc you can't reset the pointer.
+			if(braceChecker(in))	//Can only call braceChecker once per Scanner otherwise have to redefine to reset point to beginning of file
 				out.print("Braces Balanced\n");
 			else 
 				out.print("Braces Not Balanced\n");
@@ -260,35 +282,35 @@ public class IOTester {
 		out.print("\n");
 
 	//PART THREE----------------------------------------------------------------
-		//Creates Scanners for 3rd and 4th arguments
-		compareRead(args[0], args[1], out);	//Modify args[X] to compare 
+		//Compares given files 
+		compare(args[0], args[1], out);	//Modify args[X] to select which files to compare 
 		
 	//PART FOUR ----------------------------------------------------------------
 		out.print("\n");
 
 	//PART FIVE----------------------------------------------------------------
 		//5.b
-		Scanner preppedWordsFile = openWords(args[3], out, 3);
+		Scanner preppedWordsFile = openWords(args[3], out, 3);//TODO I don't want this to output "unable" if it doesn;t exist
 		Scanner adlib;
 		
-		if(preppedWordsFile != null) {
-			ArrayList<String> preppedWords = new ArrayList<String>();		//Initializes arraylist
+		if(preppedWordsFile != null) {	//TODO If null, won't it say so in output, so why proceed...  
+			ArrayList<String> preppedWords = new ArrayList<String>();		//Initializes ArrayList of prepped words
 			
 			while(preppedWordsFile.hasNextLine()) {
-				preppedWords.add(preppedWordsFile.nextLine());	//Grabs words from file and adds them to arraylist
+				preppedWords.add(preppedWordsFile.nextLine());	//Grabs words from file and adds them to preppedWords
 			}
 			
-			adlib = openWords(args[2], out, 3);						//initializes the input to search for tags
+			adlib = openWords(args[2], out, 3);					//initializes the input to search for tags
 
 			writeAdlib(preppedWords, adlib, out);				//Writes to file using prepared words
 			
 		} else {		//IF there are no preppedWords, then get user input
-			Scanner keyboard = new Scanner(System.in);
-			adlib = openWords(args[2], out, 3);
+			Scanner keyboard = new Scanner(System.in);	//Create Scanner from keyboard
+			adlib = openWords(args[2], out, 3);			//Create a scanner from the file with tags
 		
 			ArrayList<String> words = prompt(adlib, keyboard, out);	//Gets user input for tags
 			
-			adlib = openWords(args[2], out, 3);	//Resets Adlib scanner		
+			adlib = openWords(args[2], out, 3);	//Resets Adlib Scanner		
 			
 			writeAdlib(words, adlib, out);	//Writes to output using user input
 			
